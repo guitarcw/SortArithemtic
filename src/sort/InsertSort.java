fd4f615cffbf48731aeddd8a7eb2da80aae5a8c6
weic <cw_bupt@163.com>
2018-05-08 22:38:24 +0800

    插排性质

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -1,5 +1,6 @@
 package sort;
-
+//排序类型	平均情况	最好情况	最坏情况	辅助空间	稳定性
+//折半插入排序	O(n²)	 O(n)	       O(n²)	O(1)	稳定
 public class InsertSort {
     public void insertSort(int[] arr){
         if(arr==null||arr.length<=1) return;;
